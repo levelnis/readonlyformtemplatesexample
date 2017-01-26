c8ac6d28cb44ff6d2ffe603b778d263a4c826042
Simon Rigby <simon.rigby@levelnis.co.uk>
2017-01-26 07:10:10 +0000

    Added website files

---
+++ ReadOnlyViews/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace Levelnis.Learning.ReadOnlyViews.Controllers
+{
+    using System.Web.Mvc;
+    using Models;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new ReadOnlyViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ReadOnlyViewModel model)
+        {
+            model.IsReadonly = true;
+            return View(model);
+        }
+    }
+}
---
+++ ReadOnlyViews/Models/ReadOnlyViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Levelnis.Learning.ReadOnlyViews.Models
+{
+    public class ReadOnlyViewModel
+    {
+        public bool IsReadonly { get; set; }
+
+        public string PieceOfData { get; set; } 
+
+        public string AnotherPieceOfData { get; set; } 
+    }
+}   
